a6c748d45f9664f7769f1542599350022e08a547
n0mrk <impaqu@gmail.com>
2015-04-15 09:01:11 -0600

    Turtle example.

---
+++ Turtle.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+import turtle
+
+#print "Hello!"
+
+turtle.up ( )
+turtle.goto ( 0 , 35 )
+turtle.down ( )
+turtle.color ( "red" )
+turtle.fill (1)
+turtle.circle ( 20 )
+turtle.fill (0)
+turtle.up ( )
+turtle.goto ( 0 , -20 )
+turtle.down ( )
+turtle.color ( "yellow" )
+turtle.fill (1)
+turtle.circle ( 20 )
+turtle.fill (0)
+turtle.up ( )
+turtle.goto ( 0 , -75 )
+turtle.down ( )
+turtle.color ( "green" )
+turtle.fill (1)
+turtle.circle ( 20 )
+turtle.fill (0)
+
+#turtle.goto (0, 20)
+#turtle.forward ( 50 )
+#turtle.right ( 60 )
+#turtle.forward ( 50 )
+#turtle.right ( 60 )
+#turtle.forward ( 50 )
+#turtle.right ( 60 )
+#turtle.forward ( 50 )
+#turtle.right ( 60 )
+#turtle.forward ( 50 )
+#turtle.right ( 60 )
+#turtle.forward ( 50 )
+
+raw_input ( "Press return to terminate the program: " )
