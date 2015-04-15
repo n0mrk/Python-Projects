33cb373a49cd480207f329f2a66156a1b38aa07c
n0mrk <impaqu@gmail.com>
2015-04-15 08:42:05 -0600

    d

---
+++ Hello.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+print "\nHello, Python!"
+
+str1 = 'Hello World!'
+
+print str1          # Prints complete string
+print str1[0]       # Prints first character of the string
+print str1[2:5]     # Prints characters starting from 3rd to 5th
+print str1[2:]      # Prints string starting from 3rd character
+print str1* 2      # Prints string two times
+print str1 + "TEST" # Prints concatenated string
+
+del str1
+
+counter = 100          # An integer assignment
+miles   = 1000.0       # A floating point
+name    = "Mark"       # A string
+
+print "\nCounter: " + str(counter)
+print "\nMiles: " + str(miles)
+print "\nName: " + str(name)
+# or
+print "\nCounter: %d" % (counter)
+
+var1 = 1
+var2 = "Mark"
+
+print var1
+print var2
+
+var1 = 2
+del var2
+
+print var1
+#print var2  #errors because del var2
+
+#raw_input("\n\nPress the enter key to exit.")
